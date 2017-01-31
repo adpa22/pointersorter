6f10b495ac26f41a998e471a76742dd247f01f5a
adpa22 <adpa22@gmail.com>
2017-01-31 00:32:28 -0500

    Put sorting using merge sort

--- pointersorter.c
+++ pointersorter.c
@@ -31,6 +31,44 @@ int countTokens(char * input){
 
 }
 
+void  merge(int low, int mid, int high, char * a [], char * b[]){
+        int l1, l2, i;
+		for (l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++){
+                if (strcmp(a[l1], a[l2]) <= 0)
+                        b[i] = a[l1++];
+                else
+                        b[i] = a[l2++];
+
+        }
+
+        while (l1 <= mid){
+        	b[i++] = a[l1++];
+        }
+
+        while(l2 <= high){
+        	b[i++] = a[l2++];
+        }
+
+        for (i = low; i <= high; i++){
+        	a[i] = b[i];
+        }
+
+
+}
+
+
+void sort (int low, int high, char * a[], char * b[]){
+	int mid;
+	if (low < high){
+		mid = (low+high)/2;
+		sort(low,mid, a, b);
+		sort(mid+1, high, a, b);
+		merge(low,mid,high, a, b);
+	} else {
+		return;
+	}
+}
+
 
 int main (int argc, char ** argv){
 	if (argc > 2){
@@ -44,12 +82,13 @@ int main (int argc, char ** argv){
 	}
 
 	char * input = argv[1];
-	printf("INPUT: %s\n", input);
+	//printf("INPUT: %s\n", input);
 	int x = countTokens(input);
-
+	//printf("\nNumtokens: %i\n", x);
 	char * words [x];
 	int j =0;
-	for (int i = 0; i < x; i++){ //iterate words array
+	int i = 0;
+	for ( i; i < x; i++){ //iterate words array
 		
 		while(!isalpha(input[j]) && j<strlen(input)){ //skip delimators in the beginning
 			j++;
@@ -62,19 +101,30 @@ int main (int argc, char ** argv){
 		int wordEnd = index -1;
 		char * tok = (char *)malloc (wordEnd- wordStart);
 		strncpy(tok, input + wordStart, wordEnd - wordStart + 1);
-		printf("\nString: %s\n", tok);
-		printf("\nstart: %i \n end: %i\n", wordStart, wordEnd);
+		tok[wordEnd-wordStart + 1 ] = '\0';
+		//printf("\nString: %s\n", tok);
+		//printf("\nstart: %i \n end: %i\n", wordStart, wordEnd);
 		words[i] = tok;
 		
 		j = index;
 		if (j == strlen(input) || index == strlen(input)){
-			free(tok);
+			//	free(tok);
 		}
 		
 	}
-		
-	for (int test = 0; test < x; test++){
-		printf("%s\n", words[test]);
+	int test = 0;
+	for ( test; test < x; test++){
+	//	printf("%s\n", words[test]);
+	}
+	
+	char * sorted[x];
+
+	sort(0,x-1, words,sorted );
+	//printf("\n");
+	//printf("X: %i", x);
+	int fuck = 0;
+	for ( fuck; fuck < x; fuck ++){
+		printf("%s\n", sorted[fuck]);
 	}
 
 	return 0;
